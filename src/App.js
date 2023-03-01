9ffa95d21dc3282487697d2831e90afbfa540f39
Boniface Ngetich <116899434+Boniface166@users.noreply.github.com>
2023-03-01 16:00:30 +0300

    icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 //import logo from './logo.svg';
+import React from 'react'
 import { useState } from 'react';
 import './App.js';
 import './style.css';
-import  './css/bootstrap.min.css';
-import Home from "./Home"
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as Icon from 'react-bootstrap-icons';
+import Home from "./Home";
 import Scart from './Scart.js';
-import {CartProvider} from 'react-use-cart'
+import {CartProvider} from 'react-use-cart';
+
 
 
 
@@ -16,11 +19,11 @@ function App() {
          <Home/>
          <Scart/>
     </CartProvider>
+   
     
     </>
   )
      
 
-  
  }
 export default App;
--- src/Scart.js
+++ src/Scart.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useCart } from "react-use-cart";
+import * as Icon from 'react-bootstrap-icons';
 
 const SCart = () =>{
     const{ 
@@ -37,7 +38,7 @@ const SCart = () =>{
                  -
                </button>
                <button className="btn bg-primary ms-3" onClick={()=>updateItemQuantity(item.id, item.quantity+1)}>+</button>
-               <button className="btn bg-danger ms-3" onClick={()=>removeItem(item.id)}>Remove Item</button>
+               <button className="btn bg-danger ms-3" onClick={()=>removeItem(item.id)}></button>
             </td>
 
             </tr>
